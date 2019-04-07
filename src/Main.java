7121fcd360e9b1b700ad4e69aff8410fb2b95476
Denis Deniben <deniben2109@gmail.com>
2019-04-07 23:07:56 +0300

    Add some comments

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
         boolean bool = true;
-        while(bool) {
+        while(bool) { //Generating randInt
             Random random = new Random();
-            int rand = random.nextInt(100);
+            int rand = random.nextInt(100);//generate random number
             System.out.println("Загадано число от 0 до 100, угадаешь?\nВводи:");
             Scanner scan = new Scanner(System.in);
-            System.out.println(rand);
-            int number = scan.nextInt();
+            System.out.println("Загаданное число для проверки: "+rand); //If does'n need, commit
+            int number = scan.nextInt(); //input number
             String str = check(number, rand);
             System.out.println(str+"\nЗагаданное: "+rand);
-            if(str.equals("Угадал!")){
+            if(str.equals("Угадал!")){ //Condition if won
                 System.out.println("Еще раз? напиши 'Yes', если нет, то 'No'");
                 Scanner in = new Scanner(System.in);
                 String phrase = in.nextLine();
@@ -31,7 +33,7 @@ public class Main {
         }
     }
 
-    public static String check(int number, int rand) {
+    public static String check(int number, int rand) { //method which checked validation number according to rand
         if(number < 0 || number > 100){
             return "Число не попало в диапазон";
         } else if (number > rand){
