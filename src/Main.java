e882f7766509f69bf6052c93aff05936962d4a7e
Denis Deniben <deniben2109@gmail.com>
2019-04-08 12:05:42 +0300

    Fix some bugs: Check if input is number If won and say Yes play again

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,49 @@
 import java.util.Random;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
         boolean bool = true;
-        while(bool) { //Generating randInt
-            Random random = new Random();
-            int rand = random.nextInt(100);//generate random number
-            System.out.println("Загадано число от 0 до 100, угадаешь?\nВводи:");
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Загаданное число для проверки: "+rand); //If does'n need, commit
-            int number = scan.nextInt(); //input number
+        Random random = new Random();
+        int rand = random.nextInt(100);//generate random number
+        System.out.println("Загадано число от 0 до 100, угадаешь?\nВводи:");
+        while (bool) { //Generating randInt
+            Scanner scc = new Scanner(System.in);
+            int number;
+            if(!(scc.hasNextInt())){
+                System.out.println("That not a number!");
+                scc.next(); // this is important!
+            }
+            number = scc.nextInt();
             String str = check(number, rand);
-            System.out.println(str+"\nЗагаданное: "+rand);
-            if(str.equals("Угадал!")){ //Condition if won
+            System.out.println(str);
+            if (str.equals("Угадал!")) { //Condition if won
+                System.out.println(str + "\nЗагаданное: " + rand);
                 System.out.println("Еще раз? напиши 'Yes', если нет, то 'No'");
                 Scanner in = new Scanner(System.in);
                 String phrase = in.nextLine();
-                switch (phrase){
+                switch (phrase) {
                     case ("Yes"):
                         bool = true;
+                        System.out.println("Вводи:");
                         continue;
                     case ("No"):
                         bool = false;
                         break;
                 }
-                }
-                break;
+            }
         }
     }
 
     public static String check(int number, int rand) { //method which checked validation number according to rand
-        if(number < 0 || number > 100){
+        if (number < 0 || number > 100) {
             return "Число не попало в диапазон";
-        } else if (number > rand){
+        } else if (number > rand) {
             return "Меньше!";
-        } else if (number < rand){
+        } else if (number < rand) {
             return "Больше!";
-        } else if (number == rand){
+        } else if (number == rand) {
             return "Угадал!";
         }
         return "Nothing";
